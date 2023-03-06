7090dda7d371ead0d851819bc4c53b3b81094c22
rauda08 <raudajnnh08@gmail.com>
2023-03-06 16:05:34 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //Begin
+    // Numeric Luas,p,l
+    double Luas, p, l;
+    // display 'Masukan Panjang = '
+    cout << "Masukan Panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display 'Masukan Lebarnya = '
+    cout << "Masukkan Lebarnya = ";
+    // Accept 1
+    cin >> 1;
+    // Compute Luas = p*l
+    Luas = p*l;
+    // display "Luasnya = "+Luas
+    cout << 'Luasnya = " << Luas;
+    //End
+}
